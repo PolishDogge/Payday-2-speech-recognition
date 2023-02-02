159a3802d7a392cc85c44750c7957b6e0053a532
PolishDogge <75747966+PolishDogge@users.noreply.github.com>
2023-02-02 20:50:38 +0100

    added key_words to make it easier to add phrases

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ r1 = sr.Recognizer()
 mic = sr.Microphone()
  
 keyboard = Controller()
- 
+
+#if you want more things to react with it, you can add anything with; 'TEXT', so for example,  ",'surrender'" (remember, has to be lowercase)
+key_words = ['down', 'get down', 'ground', "hands up"]
+
 while True:
     with sr.Microphone() as source: #select main mic
         r.adjust_for_ambient_noise(source) #adjust
         try:
             aud = r.listen(source,timeout=3) #listen for 3 seconds
-            try: #if you want more things to react with it, you can add anything with; or 'TEXT', so for example, or 'hands up' (remember, has to be lowercase)
-               if 'down' or 'get down' or 'ground' in r.recognize_google(aud, language='en-US'):
+            try: 
+               if r.recognize_google(aud, language='en-US') in key_words:
                    keyboard.press('f')
                    sleep(0.2)
                    keyboard.release('f')
